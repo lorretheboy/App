ced969ef558866f5ffe3ec450496d2dc6b0e58bb
lorretheboy <lorretheboy@users.noreply.github.com>
2026-07-03 02:04:01 -0500

    Proposed fix for #95258

--- src/setup/platformSetup/index.native.ts
+++ src/setup/platformSetup/index.native.ts
@@ -1,1 +1,14 @@
-export default function () {}
+import DeviceInfo from 'react-native-device-info';
+
+import Log from '@libs/Log';
+
+import CONFIG from '@src/CONFIG';
+
+import pkg from '../../../package.json';
+
+export default function () {
+    const nativeVersion = DeviceInfo.getVersion();
+    if (CONFIG.IS_HYBRID_APP && !pkg.version.startsWith(nativeVersion)) {
+        Log.alert('JS bundle version does not match native binary version', {jsVersion: pkg.version, nativeVersion});
+    }
+}
