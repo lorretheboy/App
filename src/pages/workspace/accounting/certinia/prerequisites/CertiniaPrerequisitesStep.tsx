38562b2f2ed3f2d58c886212243e48cc6ba6bd1c
lorretheboy <lorretheboy@users.noreply.github.com>
2026-07-06 12:58:14 -0500

    Proposed fix for #95416

--- src/pages/workspace/accounting/certinia/prerequisites/CertiniaPrerequisitesStep.tsx
+++ src/pages/workspace/accounting/certinia/prerequisites/CertiniaPrerequisitesStep.tsx
@@ -1,6 +1,7 @@
 import Button from '@components/Button';
 import FixedFooter from '@components/FixedFooter';
 import RenderHTML from '@components/RenderHTML';
+import ScrollView from '@components/ScrollView';
 import Text from '@components/Text';
 
 import useLocalize from '@hooks/useLocalize';
@@ -35,7 +36,7 @@ function CertiniaPrerequisitesStep({onNext, currentPageName, onConnect}: Certini
     let stepContent;
     if (currentPageName === pageNames.INSTALL_BUNDLE) {
         stepContent = (
-            <View style={[styles.flex1, styles.mb3, styles.ph5]}>
+            <View style={[styles.mb3, styles.ph5]}>
                 <View>
                     <Text style={[styles.textStrong, styles.mb2]}>{translate('workspace.certinia.prerequisites.installBundlePSAHeader')}</Text>
                     <RenderHTML
@@ -58,7 +59,7 @@ function CertiniaPrerequisitesStep({onNext, currentPageName, onConnect}: Certini
         );
     } else if (currentPageName === pageNames.SETUP_CONTACTS) {
         stepContent = (
-            <View style={[styles.flex1, styles.mb3, styles.ph5]}>
+            <View style={[styles.mb3, styles.ph5]}>
                 {[
                     translate('workspace.certinia.prerequisites.setupContactsBullet1'),
                     translate('workspace.certinia.prerequisites.setupContactsBullet2'),
@@ -77,13 +78,15 @@ function CertiniaPrerequisitesStep({onNext, currentPageName, onConnect}: Certini
             </View>
         );
     } else {
-        stepContent = <Text style={[styles.flex1, styles.mb3, styles.ph5, styles.mutedTextLabel]}>{translate(descriptionKey)}</Text>;
+        stepContent = <Text style={[styles.mb3, styles.ph5, styles.mutedTextLabel]}>{translate(descriptionKey)}</Text>;
     }
 
     return (
         <View style={styles.flex1}>
-            <Text style={[styles.textHeadlineLineHeightXXL, styles.ph5, styles.mb3]}>{translate(titleKey)}</Text>
-            {stepContent}
+            <ScrollView style={styles.flex1}>
+                <Text style={[styles.textHeadlineLineHeightXXL, styles.ph5, styles.mb3]}>{translate(titleKey)}</Text>
+                {stepContent}
+            </ScrollView>
             <FixedFooter
                 style={[styles.mtAuto]}
                 addBottomSafeAreaPadding
